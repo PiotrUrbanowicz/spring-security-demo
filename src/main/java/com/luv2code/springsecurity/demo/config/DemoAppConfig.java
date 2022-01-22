3cf791e4d72580c91dda096e0d779f6c0d43b7f0
GitPiotrUrbanowicz <55048144+piotr1202@users.noreply.github.com>
2022-01-22 18:04:52 +0100

    08-jdbc-plaintext
    Reading data from properties file and using it to connect to our database.
    And form there taking information.

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,19 +1,36 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 	
-	//define a bean for ViewResolver
+	@Autowired
+	private Environment env;// holds the date read from the properties file.
+	
+	private Logger logger =Logger.getLogger(getClass().getName());
+	
+	
 	
 	@Bean
 	public ViewResolver viewResolver() {
@@ -23,6 +40,51 @@ public class DemoAppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource securityDataSource() {
+		System.out.println("kok");
+		ComboPooledDataSource securityDataSource 
+							= new ComboPooledDataSource();
+		
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		logger.info(">>> jdbc.url="+ env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user="+ env.getProperty("jdbc.user"));
+		
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		securityDataSource.setInitialPoolSize(
+				getIntProperty("connection.pool.initialPoolSize"));
+		
+		securityDataSource.setMinPoolSize(
+				getIntProperty("connection.pool.minPoolSize"));
+		
+		securityDataSource.setMaxPoolSize(
+				getIntProperty("connection.pool.maxPoolSize"));
+		
+		securityDataSource.setMaxIdleTime(
+				getIntProperty("connection.pool.maxIdleTime"));
+		
+		
+		
+		return securityDataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		String propVal= env.getProperty(propName);
+		
+		int intPropVal=Integer.parseInt(propVal);
+		
+		return intPropVal;
+		
 	}
 
 }
